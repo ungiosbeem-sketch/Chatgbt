263fdcd97ff1c32f48bb9d266a8f7bb7a53f13af
ungiosbeem-sketch <ungiosbeem@gmail.com>
2026-06-08 11:35:21 +0300

    Create _layout.tsx

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+import { Provider } from "react-redux";
+import { store } from "../src/store";
+
+export default function Layout() {
+  return (
+    <Provider store={store}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </Provider>
+  );
+}
