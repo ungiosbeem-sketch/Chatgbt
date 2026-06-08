13f058d7f6917d9c4b64c58286075421a822a89e
ungiosbeem-sketch <ungiosbeem@gmail.com>
2026-06-08 11:55:59 +0300

    Create authValidation.ts

---
+++ src/utils/authValidation.ts
@@ -0,0 +1,25 @@
+import * as yup from "yup";
+
+export const loginSchema = yup.object({
+  email: yup
+    .string()
+    .email()
+    .required(),
+
+  password: yup
+    .string()
+    .min(6)
+    .required(),
+});
+
+export const registerSchema = yup.object({
+  email: yup
+    .string()
+    .email()
+    .required(),
+
+  password: yup
+    .string()
+    .min(6)
+    .required(),
+});
