929be8d80d9b55702565dea6bc57b647f83c1d59
ungiosbeem-sketch <ungiosbeem@gmail.com>
2026-06-08 12:08:35 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,41 @@
-import { Redirect } from "expo-router";
+import { useEffect } from "react";
+import { router } from "expo-router";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+} from "react-native";
 
-export default function Index() {
-  return <Redirect href="/(auth)/login" />;
+export default function Splash() {
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/onboarding");
+    }, 2500);
+  }, []);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#3366FF",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          color: "#fff",
+          fontWeight: "700",
+        }}
+      >
+        Abdalla Keynan
+      </Text>
+
+      <ActivityIndicator
+        size="large"
+        color="#fff"
+      />
+    </View>
+  );
 }
